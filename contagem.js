9f8fcb11c3c8645e178b5476b1d78b4ccf34f4b3
Elbert Silva <80282612+elbertsilva@users.noreply.github.com>
2021-09-15 08:57:24 -0300

    Add files via upload

---
+++ contagem.js
@@ -0,0 +1,9 @@
+var contagem = 80;
+
+if (contagem > 80) {
+  console.log("Estoque alto ");
+} else if (contagem < 80) {
+  console.log("Estoque médio");
+} else {
+  console.log("estoque baixo");
+}
