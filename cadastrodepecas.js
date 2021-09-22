15dfe3569e86e2f714fbc3d49d39d7605e9290c5
Gabriele dos Santos Oliveira <90703796+santosgabriele@users.noreply.github.com>
2021-09-21 21:36:08 -0300

    Add files via upload

--- cadastrodepecas.js
+++ cadastrodepecas.js
@@ -1,33 +1,33 @@
-    var ListaDePecas = ["AB", "Peça 2", "peça 3", "peça 4", "peça 5", "peça 6", "peça 7", "peça 8", "peça 9", "peça 10", "peça 11"]
-    console.log("quantidade de peças");
-//A linha de cima é apenas um informação apenas.
+    var ListadePecas = ["AB" , "Peças 2", "Peças 3" , "Peças 4" , "Peças 5" , "Peças 6" , "Peças 7" , "Peças 8" , "Peças 9" , "Peças 10" , "Peças 11"]
+    console.log("Quantidade de peças");
+    //A linha de cima é uma informção apenas.
 
-if(ListaDePecas.length > 10 ){
-    console.log ("capacidade insuficiente");
-} else {
-    console.log("as peças podem ser cadastradas")
+if (ListadePecas.length > 10 ){
+    console.log("Capacidade insuficiente");
+
+}else {
+    console.log("As peças podem ser cadastradas")
 }
 
+console.log("Quantidade de caracteres");
 
-console.log ("quantidade de caracteres");
-//O USO DO [] serve apena para falar de uma aposição
-for (var contador = 0; contador < ListaDePecas.length; contador++ ){
-    var pecaAtual = ListaDePecas [contador];
+// Criamos a variavel contador =0, sempre que a variavel for menor que alista de peças, o contador ++ sempre adiciona mais 1.
+for (var contador = 0; contador < ListadePecas.length; contador++){
+    var PecaAtual = ListadePecas[contador];
 
-    if (pecaAtual.length < 3 ) {
-        console.log (pecaAtual + ": a peca possui nome inferior a 3 caracteres e não poder ser cadastrada")
-    }
-    else {
-        console.log(pecaAtual + ": a peca pode ser cadastrada")
-    }
+if (PecaAtual.length < 3){
+console.log(PecaAtual + ": a peça possui nome inferior a 3 caracteres e não pode ser cadastrada");
 }
+else {
+console.log(PecaAtual + " a peça pode ser cadastrada")
+}
+}
+console.log("Peso da peça");
 
-console.log ("Peso da peça");
+var PesodaPecaemGramas =50;
 
-var PesoDaPecaEmGramas = 50;
-
-    if (PesoDaPecaEmGramas > 100){
-        console.log ("peso insuficiente");
-    } else{ 
-        console.log ("peso suficiente")
-    }
+if (PesodaPecaemGramas >100){
+    console.log("peso insulficiente");
+} else{
+    console.log("pelo suficiente")
+}
