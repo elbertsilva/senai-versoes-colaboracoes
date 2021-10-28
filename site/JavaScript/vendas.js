047873a37cd138c807b1c53edd207d6e1b92055a
elbertsilva <elberteduardomartins@gmail.com>
2021-10-28 15:15:21 -0300

    criando vendas

--- site/JavaScript/vendas.js
+++ site/JavaScript/vendas.js
@@ -1,4 +1,11 @@
 function bt1() {
   //   alert("ola mundo");
-  document.querySelector(".popup").style.display = "block";
+  document.querySelector(".popup").style.display = "grid";
+}
+$("#cep").mask("99/99/9999");
+// $(document).ready(function () {
+//   $("#cep").mask("99.999-999");
+// });
+function bt2() {
+  document.querySelector(".popup2").style.display = "grid";
 }
